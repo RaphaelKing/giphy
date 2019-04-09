4b3f4dc0f36ad61b7cd194c49b459b187dab5190
raphaelking <andreslater23@gmail.com>
2019-04-09 21:57:51 +0000

    ^

--- js/script.js
+++ js/script.js
@@ -4,8 +4,25 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+    var input= $("input").val ()
+    var request_url ="https://api.giphy.com/v1/gifs/search?q="+ input + "&rating=pg&api_key=dc6zaTOxFJmzC";
+         console.log(request_url);
+         
+         function getRandomNumb(min,max) {
+             var request = Math.floor(Math.random() * (max-min)) + min; //The maximum is exclusive and the minimum is inclusive
+             
+             
+             console.log(request);
+             return request;
+         }
+    
+    $.ajax({
+      url: request_url,
+      method: "GET",
+      success: function(response) {
+           var pic_url = response.data[0].images.original.url;
+           $('body').append('<img src=' + pic_url + '>');
+      },
+    }); 
+    
 });
-
